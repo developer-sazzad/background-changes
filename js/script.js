6276b353532c33a7233ac4e9044fffb170a66ed1
developer-sazzad <developersazzad@gmail.com>
2024-07-11 17:10:57 +0600

    background and text color changes with JS

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+
+document.getElementById('make-blue').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'blue';
+    if (backgroundColor = 'blue') {
+        document.getElementById('title').style.color = "red";
+    }
+})
+
+document.getElementById('make-black').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'black';
+    if (backgroundColor = 'blue') {
+        document.getElementById('title').style.color = "white";
+    }
+})
+
+document.getElementById('make-green').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'green';
+    if (backgroundColor = 'green') {
+        document.getElementById('title').style.color = "black";
+    }
+})
+
